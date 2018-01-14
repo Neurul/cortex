1f72bd8a4fad8186c4c995fb4bfb708f584800b8
Elmer Roderick Bool <elmer.bool@ei8.works>
2018-01-14 13:18:29 +1100

    Transferred Self-hosting code to Common.

--- src/main/Port.Adapter/Admin/Cli/Program.cs
+++ src/main/Port.Adapter/Admin/Cli/Program.cs
@@ -1,5 +1,8 @@
-﻿using Nancy.Hosting.Self;
+﻿using Nancy.Bootstrapper;
+using Nancy.Hosting.Self;
+using org.neurul.Common;
 using org.neurul.Common.Domain.Model;
+using org.neurul.Common.Http.Cli;
 using System;
 
 namespace org.neurul.Cortex.Port.Adapter.Admin.Cli
@@ -10,55 +13,18 @@ namespace org.neurul.Cortex.Port.Adapter.Admin.Cli
         {
             try
             {
-                Console.WriteLine();
-                Console.WriteLine("Initializing Neurul Cortex...");
-                Console.WriteLine();
-
-                AssertionConcern.AssertArgumentValid(a => a != null && a.Length == 2, args, "Must specify 'In' and 'Out' URIs.", nameof(args));
-                AssertionConcern.AssertArgumentValid(a => Uri.IsWellFormedUriString(a[0], UriKind.RelativeOrAbsolute), args, "Must specify valid 'In' URI", nameof(args));
-                AssertionConcern.AssertArgumentValid(a => Uri.IsWellFormedUriString(a[1], UriKind.RelativeOrAbsolute), args, "Must specify valid 'Out' URI", nameof(args));
-
-                var host = new NancyHost(new In.Http.CustomBootstrapper(), new Uri(args[0]));
-                host.Start();
-
-                host = new NancyHost(new Out.Http.CustomBootstrapper(), new Uri(args[1]));
-                host.Start();
-
-                var response = string.Empty;
-
-                while (response.ToUpper() != "Y")
-                {
-                    response = string.Empty;
-
-                    Console.Clear();
-                    Console.WriteLine($"In: {args[0]}");
-                    Console.WriteLine($"Out: {args[1]}");
-                    Console.WriteLine();
-
-                    Console.WriteLine("Neurul Cortex online.");
-                    Console.WriteLine();
-                    Console.WriteLine("Enjoy!");
-                    Console.WriteLine();
-
-                    Console.Write("Press any key to exit...");
-                    Console.ReadKey(true);
-                    Console.WriteLine();
-                    Console.WriteLine();
-                    Console.WriteLine("Are you sure you wish to exit? (Y/N)");
-                    while (response == string.Empty)
-                    {
-                        response = Console.ReadKey(true).KeyChar.ToString();
-                        if (response.ToUpper() != "Y" && response.ToUpper() != "N")
-                        {
-                            response = string.Empty;
-                            Console.Beep();
-                        }
-                    }
-                }
+                MultiHostProgram.Start(
+                    new DefaultConsoleWrapper(),
+                    "Neurul Cortex",
+                    args,
+                    new string[] { "In", "Out" },
+                    new INancyBootstrapper[] { new In.Http.CustomBootstrapper(), new Out.Http.CustomBootstrapper() }
+                    );
             }
             catch (Exception ex)
             {
                 Console.WriteLine("The following exception occurred:");
+                Console.WriteLine();
                 Console.WriteLine(ex.Message);
             }
         }
